e4503ddae8544e67c7dd6b347b499562763d3773
Zachary Cutler <zacharycutler@Zacharys-MacBook-Pro.local>
2020-05-02 13:15:52 -0600

    fixed merge

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import {createMap} from './map.js'
 import {createLineChart} from './lineChart.js'
 
-d3.json("slc.json").then(function(slcData){
-  d3.json("denver.json").then(function(denverData){
-    d3.json("la.json").then(function(laData){
-      d3.json("minneapolis.json").then(function(minneapolisData){
+d3.json("data/slc.json").then(function(slcData){
+  d3.json("data/denver.json").then(function(denverData){
+    d3.json("data/la.json").then(function(laData){
+      d3.json("data/minneapolis.json").then(function(minneapolisData){
         createMap(slcData);
         createLineChart(slcData, denverData, laData, minneapolisData);
       });
